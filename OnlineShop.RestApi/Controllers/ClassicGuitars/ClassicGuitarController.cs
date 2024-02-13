ee2f7a51f80d3c1f73e4d954acdd0b1d5da43186
Alirza-Vln <alirezavaladan74@gmail.com>
2024-02-13 11:49:44 +0330

    refactor-for-clean-architecture

---
+++ OnlineShop.RestApi/Controllers/ClassicGuitars/ClassicGuitarController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OnlineShopGuitar.DTO;
+using OnlineShopGuitar.Entities;
+using OnlineShopGuitar.Services;
+using OnlineShopGuitar.Services.Contracts;
+
+namespace OnlineShopGuitar.Controllers
+{
+    [ApiController]
+    [Route("ClassicGuitar")]
+    public class ClassicGuitarController : Controller
+    {
+    private readonly IClassicGuitarService _service;
+        public ClassicGuitarController(IClassicGuitarService service)
+        {
+            _service = service;
+        }
+        [HttpPost("Add-ClassiceGuitar")]
+
+        public void AddClassic([FromBody]AddClassicGuitarDto dto)
+        {
+            _service.AddClassic(dto);
+        }
+
+        [HttpGet("GetAllClassicGuitar")]
+
+        public List<ClassicGuitar> GetClassic()
+        {
+            return _service.GetClassicGuitars();
+        }
+
+        [HttpDelete("Delete-classicGuitar")]
+
+        public List<ClassicGuitar> DeleteClassic([FromQuery]DeleteClassicGuitarDto dto)
+        {
+            return _service.DeleteClassicGuitars(dto);
+        }
+
+        [HttpPatch("Upadte-CalassicGuitarPrice")]
+
+        public  void UpdateClassicPrice([FromQuery]UpdateClassicGuitarDto dto)
+        {
+            _service.UpdateClassicGuitar(dto);
+        }
+    }
+}
